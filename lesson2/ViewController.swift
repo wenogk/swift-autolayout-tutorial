b2b555741d627cfb604a16f23c88ddafff4a21d0
wenogk <drf325@nyu.edu>
2020-07-05 23:32:46 +0400

    Initial Commit

---
+++ lesson2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson2
+//
+//  Created by Romeno Wenogk Fernando on 05/07/2020.
+//  Copyright © 2020 Romeno Wenogk Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
